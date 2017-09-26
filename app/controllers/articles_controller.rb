9b83610283993fcc4732451bcebb54538e38c86a
Luong Vo <vo.tran.thanh.luong@gmail.com>
2017-09-26 13:51:39 +0700

    Add delete article function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,22 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index 
     @articles = Article.all
   end
   def show 
     @article = Article.find(params[:id])
   end
+  def new 
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(@article.params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+  def destroy 
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to article_path(@article)
+  end 
 end
